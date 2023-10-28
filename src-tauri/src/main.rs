022e1a56147a4a0aea2e9649952570d3a308e42d
Uncle-Simon <88356557+Uncle-Simon@users.noreply.github.com>
2023-10-27 21:55:31 -0500

    Added Skip, fixed null range, added floats

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use evalexpr::*;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn get_answer(answer: &str, question: &str) -> (String, bool) {
-    let x = eval_int(question).unwrap();
+    let x: f64 = eval_float(question).unwrap();
     if answer == x.to_string() {
         (format!("{} is correct! Good job!", answer), true)
     }
@@ -17,27 +17,37 @@ fn get_answer(answer: &str, question: &str) -> (String, bool) {
 }
 
 #[tauri::command]
-fn get_new_question(range: String) -> String {
-    let mut rng = rand::thread_rng();
+fn skip_question(question: &str, range: String) -> (f64, String) {
+    let x: f64 = eval_float(question).unwrap();
+    let y = get_new_question(range);
+    (x, y)
+}
 
-    let range_num = eval(&range).unwrap().as_int().unwrap();
+#[tauri::command]
+fn get_new_question(range: String) -> String {
+    let mut rng: rand::rngs::ThreadRng = rand::thread_rng();
+
+    let mut range_num: i64 = eval(&range).unwrap().as_int().unwrap();
+
+    range_num = range_num.clamp(2, 1000000000);
 
     let num1: i64 = rng.gen_range(-range_num..range_num);
     let mut num2: i64 = rng.gen_range(-range_num..range_num);
+
     let operand_array = ["+", "-", "*", "/"];
 
     let operand: usize = rng.gen_range(0..operand_array.len());
 
-    if operand == 3 && num2 == 0 {
-        num2 += 1;
+    if operand_array[operand] == "/" && num2 == 0 {
+        num2 += rng.gen_range(1..range_num);
     }
     
-    format!("{} {} {}", num1, operand_array[operand], num2)
+    format!("{}.0 {} {}.0", num1, operand_array[operand], num2)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_answer, get_new_question])
+        .invoke_handler(tauri::generate_handler![get_answer, get_new_question, skip_question])
         .run(tauri::generate_context!())
         .expect("error while running tauri application")
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let mathInputEl;
 let mathCorrectEl;
 let mathQuestionEl;
 let rangeEl;
+let positiveEl;
 
 async function submitAnswer() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -20,7 +21,18 @@ async function submitAnswer() {
 }
 
 async function newQuestion() {
-  mathQuestionEl.textContent = await invoke("get_new_question", { range: rangeEl.value });
+  mathQuestionEl.textContent = await invoke("get_new_question", {
+    range: rangeEl.value,
+  });
+}
+
+async function skipQuestion() {
+  let answers = await invoke("skip_question", {
+    question: mathQuestionEl.textContent,
+    range: rangeEl.value,
+  });
+  mathCorrectEl.textContent = "The correct answer was: " + answers[0];
+  mathQuestionEl.textContent = answers[1];
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -28,12 +40,22 @@ window.addEventListener("DOMContentLoaded", () => {
   mathCorrectEl = document.querySelector("#math-correct");
   mathQuestionEl = document.querySelector("#math-question");
   rangeEl = document.querySelector("#range");
+  positiveEl = document.querySelector("#positive");
   newQuestion();
   document.querySelector("#math-form").addEventListener("submit", (e) => {
     e.preventDefault();
     submitAnswer();
   });
+  document.getElementById("range").addEventListener("change", function () {
+    let v = parseInt(this.value);
+    if (v > 2 && v < 1000000000) return
+    else if (v > 1000000000) this.value = 1000000000;
+    else this.value = 2;
+  });
   document.querySelector("#range").addEventListener("change", (e) => {
     newQuestion();
   });
+  document.querySelector("#skip").addEventListener("click", (e) => {
+    skipQuestion();
+  })
 });
